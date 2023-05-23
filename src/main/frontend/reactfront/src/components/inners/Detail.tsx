81583c9aebe22ccd5283e3dbdeb60125ef29b908
JiminKim <godjimin001225@gmail.com>
2023-05-23 17:19:57 +0900

    0523update(animation update)

--- src/main/frontend/reactfront/src/components/inners/Detail.tsx
+++ src/main/frontend/reactfront/src/components/inners/Detail.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import "./Detail.css"
 import exp from "constants";
-// @ts-ignore
-import Fade from "react-reveal/Fade"
-const detail = () => {
+import useScrollFadeIn from "../../hooks/useScrollFadeIn";
+const Detail = () => {
+        const animatedItem = useScrollFadeIn();
+
         return (
             <div>
                 <div className="detail bg-2">
-                    <div className="detailtext">
+                    <div className="detailtext" {...animatedItem}>
                             평소 컨텐츠를 통해 소비하던 캐릭터들과 직접 대화해 보세요.<br/>
                             MAF의 캐릭터들은 여러분들과의 대화를 기다리고 있어요.<br/>
                             어떤 말을 하든 잘 들어주고 대답해줄거에요.<br/>
@@ -17,4 +18,4 @@ const detail = () => {
         );
 }
 
-export default detail
+export default Detail
--- src/main/frontend/reactfront/src/components/inners/Sitefunc.tsx
+++ src/main/frontend/reactfront/src/components/inners/Sitefunc.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import "./Sitefunc.css";
 import exp from "constants";
 import chatting from "../../img/chatting.gif"
-const sitefunc = () => {
+import useScrollFadeIn from "../../hooks/useScrollFadeIn";
+const Sitefunc = () => {
+        const animatedItem = useScrollFadeIn();
+
         return (
             <div className="sitefunc bg-3">
                 <div className="site1">
                         ⚙︎기능 구현 방법
                 </div>
-                <div className="site2">
+                <div className="site2" {...animatedItem}>
                         MAF의 챗 봇들을 구글 Colab을 사용하여 <br/>
                         각 봇들의 데이터를 학습시켜 데이터 모델들을 생성합니다.     <br/>
                         그 후, Hugging Face 플랫폼을 통해      <br/>
@@ -16,11 +19,11 @@ const sitefunc = () => {
                         그렇게 챗 봇들을 MAF 사이트로 불러와 <br/>
                         이용자 여러분들과 대화할 수 있도록 만들었습니다. <br/>
                 </div>
-                <div className="sitepic">
+                <div className="sitepic" >
                         <img src={chatting} className="chatpic"/>
                 </div>
             </div>
         );
 }
 
-export default sitefunc
+export default Sitefunc
